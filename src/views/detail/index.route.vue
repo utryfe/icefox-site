c1fdbfe194106c6740cdae2beccb50f8bbf0afcf
Della156 <della_i@163.com>
2019-07-10 14:59:39 +0800

    feat: split分割

--- src/views/detail/index.route.vue
+++ src/views/detail/index.route.vue
@@ -1,7 +1,15 @@
 <template>
-  <ice-layout>
-    <div style="height: 300px; width: 300px; background-color: #ccc;">11</div>
-  </ice-layout>
+  <ice-split-panes max-size="70%">
+    <div slot="left">A</div>
+    <ice-split-panes slot="right" horizontal>
+      <ice-split-panes slot="top">
+        <div slot="left">B</div>
+        <div slot="right">C</div>
+      </ice-split-panes>
+
+      <div slot="bottom">D</div>
+    </ice-split-panes>
+  </ice-split-panes>
 </template>
 
 <script>
