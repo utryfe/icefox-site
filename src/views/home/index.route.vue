ab2913b257ab30a2371340abe9cbeada8350e215
Della156 <della_i@163.com>
2019-07-19 17:37:19 +0800

    feat: 修改首页

--- src/views/home/index.route.vue
+++ src/views/home/index.route.vue
@@ -8,6 +8,16 @@
 export default {
   name: 'homePage',
   title: '首页',
-  // nocache: false,
+  nocache: false,
 }
 </script>
+<style lang="less" scoped>
+.home-page {
+  height: 300px;
+  width: 300px;
+  line-height: 300px;
+  text-align: center;
+  font-size: 70px;
+  margin: 100px auto;
+}
+</style>
